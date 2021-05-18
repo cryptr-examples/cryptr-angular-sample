389c08aea65cf3997492c23f53789e8dce82582d
Melanie Flandrin <melanie.flandrin@gmail.com>
2021-05-18 10:45:23 +0200

    Modify env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,16 @@ export const environment = {
   cryptrConfig: {
     audience: 'http://localhost:4200',
     tenant_domain: 'shark-academy',
-    client_id: 'ab76295e-39d2-479b-ad06-eda1564dc729',
-    default_redirect_uri: 'http://localhost:4200/',
-    // region: 'us',
-    cryptr_base_url: 'https://cleeck-umbrella-staging-eu.onrender.com',
-    default_locale: 'fr',
+    client_id: '5f43f8ac-2591-4a01-b69d-acfadb9b3187',
+    region: 'eu',
+    default_redirect_uri: 'http://localhost:4200',
     httpInterceptor: {
       apiRequestsToSecure: ['http://localhost:5000/*'],
     },
     telemetry: false,
   },
   resource_server_url: 'http://localhost:5000',
-};
+} 
+
+
+                
