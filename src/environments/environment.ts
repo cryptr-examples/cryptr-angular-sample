d4aaf570a324d805b9017df72363f27ddaedddb7
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-08-04 15:47:16 +0200

    update environment structure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,14 @@ export const environment = {
     audience: 'http://localhost:4200',
     tenant_domain: 'shark-academy',
     client_id: '5f43f8ac-2591-4a01-b69d-acfadb9b3187',
-    region: 'eu',
+    cryptr_base_url: 'https://samly.howto:4433',
     default_redirect_uri: 'http://localhost:4200',
     httpInterceptor: {
       apiRequestsToSecure: ['http://localhost:5000/*'],
     },
     telemetry: false,
+    dedicated_server: true,
+    fixed_pkce: true
   },
   resource_server_url: 'http://localhost:5000',
-} 
+}
